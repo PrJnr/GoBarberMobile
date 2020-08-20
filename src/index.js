106cc7dfb80f5537f1d51fd12e6209138be304a6
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-08-20 20:26:07 -0300

    Config root import + react navigation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
 
+import Routes from './routes'
 
 
 // import { Container } from './styles';
 
 const src = () => {
-  return <View />;
+  return (
+      <NavigationContainer>
+          <Routes/>
+      </NavigationContainer>
+  )
+
 }
 
 export default src;
