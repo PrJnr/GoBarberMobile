e0ce2362a17b460b653577b2f249efbf701b6d6c
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-08-20 20:47:20 -0300

    background com linear gradient + styled component

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import Background from '~/components/background'
+
 // import { Container } from './styles';
 
 const SignUp = () => {
-  return <View />;
+  return (
+      <Background>
+          <View />
+
+      </Background>
+  );
 }
 
 export default SignUp;
