51a48d60a76e47f0abf41c4fd40ed45189ce1f70
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-08-24 20:51:29 -0300

    add statusBar

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native'
+import {StatusBar, View} from 'react-native';
 
-import Routes from './routes'
+import {NavigationContainer} from '@react-navigation/native';
 
+import Routes from './routes';
 
 // import { Container } from './styles';
 
 const src = () => {
-  return (
-      <NavigationContainer>
-          <Routes/>
-      </NavigationContainer>
-  )
-
-}
+    return (
+        <NavigationContainer>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+            <Routes />
+        </NavigationContainer>
+    );
+};
 
 export default src;
