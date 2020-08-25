8d381c02a53a6cab8d104dfd5a51246817487671
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-08-24 21:03:03 -0300

    add funções de acessibildiade

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,49 +1,67 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, {useRef} from 'react';
+import {Image} from 'react-native';
 
-import logo from '~/assets/logo.png'
+import logo from '~/assets/logo.png';
 
+import Background from '~/components/background';
 
-import Background from '~/components/background'
-
-import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles';
+import {
+    Container,
+    Form,
+    FormInput,
+    SubmitButton,
+    SignLink,
+    SignLinkText,
+} from './styles';
 
 export default function SignUp({navigation}) {
-  return (
-      <Background>
-          <Container>
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    function handleSubmit() {}
+    return (
+        <Background>
+            <Container>
                 <Image source={logo} />
                 <Form>
-                        <FormInput icon="person-outline"
-
+                    <FormInput
+                        icon="person-outline"
                         autoCorrect={false}
                         autoCapitalize="none"
                         placeholder="Nome Completo"
-                        />
-                        <FormInput icon="mail-outline"
+                        returnKeyType="next"
+                        onSubmitEditing={() => emailRef.current.focus()}
+                    />
+                    <FormInput
+                        icon="mail-outline"
                         keyboard="email-address"
                         autoCorrect={false}
                         autoCapitalize="none"
                         placeholder="Digite seu e-mail"
-                        />
-                        <FormInput icon="lock-outline"
+                        ref={emailRef}
+                        returnKeyType="next"
+                        onSubmitEditing={() => passwordRef.current.focus()}
+                    />
+                    <FormInput
+                        icon="lock-outline"
                         secureTextEntry
-
                         placeholder="Digite sua senha secreta"
-                        />
+                        ref={passwordRef}
+                        returnKeyType="send"
+                        onSubmitEditing={handleSubmit}
+                    />
 
-                        <SubmitButton onPress={()=> {}}>
-                            Cadastre-se
-                        </SubmitButton>
+                    <SubmitButton onPress={handleSubmit}>
+                        Cadastre-se
+                    </SubmitButton>
                 </Form>
-                <SignLink onPress={()=> {navigation.navigate('SignIn')}} >
-                    <SignLinkText>Acessar minha conta</SignLinkText>
+                <SignLink
+                    onPress={() => {
+                        navigation.navigate('SignIn');
+                    }}>
+                    <SignLinkText>Já tenho conta</SignLinkText>
                 </SignLink>
-
-          </Container>
-
+            </Container>
         </Background>
-  );
+    );
 }
-
-
