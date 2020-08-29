c86e94577cbcd97807be34940581ae92866335aa
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-08-29 18:42:54 -0300

    Add DatePicker p/ Android

--- src/components/DateInput/index.android.js
+++ src/components/DateInput/index.android.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useMemo} from 'react';
+import {format} from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import {DatePickerAndroid} from 'react-native';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const DateInput = () => {
-    return <View />;
+import {Container, DateButton, DateText} from './styles';
+
+const DateInput = (date, onChange) => {
+    const dateFormatted = useMemo(
+        () => format(date, "dd 'de' MMMM 'de' yyyy", {locale: pt}),
+        [date],
+    );
+
+    async function handleOpenPicker() {
+        const {action, year, month, day} = await DatePickerAndroid.open({
+            mode: 'spinner',
+            date,
+        });
+
+        if (action === DatePickerAndroid.dateSetAction) {
+            const selectedDate = new Date(year, month, day);
+
+            onChange(selectedDate);
+        }
+    }
+
+    return (
+        <Container>
+            <DateButton onPress={handleOpenPicker}>
+                <Icon name="event" color="#fff" size={20} />
+                <DateText>{dateFormatted}</DateText>
+            </DateButton>
+        </Container>
+    );
 };
 
 export default DateInput;
