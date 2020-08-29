733f0ced4fc79a07eb51738c1599750a31e56eed
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-08-29 17:21:18 -0300

    estilizando a Pgna do Dashboard

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,130 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '~/components/background';
-// import { Container } from './styles';
+import {updateProfileRequest} from '~/store/modules/user/actions';
+import {signOut} from '~/store/modules/auth/actions';
+
+import {
+    Container,
+    Title,
+    Form,
+    FormInput,
+    SubmitButton,
+    Separator,
+    LogoutButton,
+} from './styles';
 
 const Profile = () => {
-    return <Background />;
+    const profile = useSelector((state) => state.user.profile);
+    const dispatch = useDispatch();
+
+    const emailRef = useRef();
+    const oldPasswordRef = useRef();
+    const passwordRef = useRef();
+    const confirmPasswordRef = useRef();
+
+    const [name, setName] = useState(profile.name);
+    const [email, setEmail] = useState(profile.email);
+    const [oldPassword, setOldPassword] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    useEffect(() => {
+        setOldPassword('');
+        setPassword('');
+        setConfirmPassword('');
+    }, [profile]);
+
+    function handleSubmit() {
+        dispatch(
+            updateProfileRequest({
+                name,
+                email,
+                oldPassword,
+                password,
+                confirmPassword,
+            }),
+        );
+    }
+
+    function handleLogout() {
+        dispatch(signOut());
+    }
+
+    return (
+        <Background>
+            <Container>
+                <Title>Meu Perfil</Title>
+
+                <Form>
+                    <FormInput
+                        icon="person-outline"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Nome Completo"
+                        returnKeyType="next"
+                        onSubmitEditing={() => emailRef.current.focus()}
+                        value={name}
+                        onChangeText={setName}
+                    />
+                    <FormInput
+                        icon="mail-outline"
+                        keyboard="email-address"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Digite seu e-mail"
+                        ref={emailRef}
+                        returnKeyType="next"
+                        onSubmitEditing={() => oldPasswordRef.current.focus()}
+                        value={email}
+                        onChangeText={setEmail}
+                    />
+                    <Separator />
+
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Sua senha Atual"
+                        ref={oldPasswordRef}
+                        returnKeyType="next"
+                        onSubmitEditing={() => passwordRef.current.focus()}
+                        value={oldPassword}
+                        onChangeText={setOldPassword}
+                    />
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Sua nova senha"
+                        ref={passwordRef}
+                        returnKeyType="next"
+                        onSubmitEditing={() =>
+                            confirmPasswordRef.current.focus()
+                        }
+                        value={password}
+                        onChangeText={setPassword}
+                    />
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Confirmação de senha"
+                        ref={confirmPasswordRef}
+                        returnKeyType="send"
+                        onSubmitEditing={handleSubmit}
+                        value={confirmPassword}
+                        onChangeText={setConfirmPassword}
+                    />
+                    <SubmitButton onPress={handleSubmit}>
+                        Atualizar Perfil
+                    </SubmitButton>
+                    <LogoutButton onPress={handleLogout}>
+                        Desconectar
+                    </LogoutButton>
+                </Form>
+            </Container>
+        </Background>
+    );
 };
 
 export default Profile;
