4eacb861d2e4dcfc1dd05a8caefc2aaa6d2adc0e
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-08-29 17:46:48 -0300

    Page de SelectProvider p/ add um novo agendamento

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,13 +1,53 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, {useEffect, useState} from 'react';
 import {TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/background';
 
-// import { Container } from './styles';
+import api from '~/services/api';
 
-const SelectProvider = () => {
-    return <Background />;
+import {Container, ProvidersList, Provider, Avatar, Name} from './styles';
+
+const SelectProvider = ({navigation}) => {
+    const [providers, setProviders] = useState([]);
+
+    useEffect(() => {
+        async function loadProviders() {
+            const response = await api.get('providers');
+            setProviders(response.data);
+        }
+        loadProviders();
+    }, []);
+    return (
+        <Background>
+            <Container>
+                <ProvidersList
+                    data={providers}
+                    keyExtractor={(provider) => String(provider.id)}
+                    renderItem={(
+                        {item}, // Item contem todos os dados de Provider
+                    ) => (
+                        <Provider
+                            onPress={() =>
+                                navigation.navigate('SelectDateTime', {
+                                    item,
+                                })
+                            }>
+                            <Avatar
+                                source={{
+                                    uri: item.provider.avatar
+                                        ? item.provider.avatar.url
+                                        : `https://api.adorable.io/avatar/50/${item.name}.png`,
+                                }}
+                            />
+                            <Name>{item.name}</Name>
+                        </Provider>
+                    )}
+                />
+            </Container>
+        </Background>
+    );
 };
 
 export default SelectProvider;
