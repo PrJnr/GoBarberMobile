9ebdff204e67f49f753728d79cfbd5ceee40c665
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-08-29 17:20:59 -0300

    Criando page/component p/ listar os agendamentos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,60 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import api from '~/services/api';
+
 import Background from '~/components/background';
-// import {Container} from './styles';
+import {Container, Title, List} from './styles';
+
+import Appointment from '~/components/appointment';
 
 const Dashboard = () => {
-    return <Background />;
+    const [appointments, setAppointments] = useState([]);
+    useEffect(() => {
+        async function loadAppointments() {
+            const response = await api.get('appointments');
+
+            setAppointments(response.data);
+        }
+
+        loadAppointments();
+    }, []);
+
+    async function handleCancel(id) {
+        const response = await api.delete(`appointments/${id}`);
+
+        setAppointments(
+            appointments.map((appointment) =>
+                appointment.id === id
+                    ? {
+                          ...appointment,
+                          canceled_at: response.data.canceled_at,
+                      }
+                    : appointment,
+            ),
+        );
+    }
+
+    return (
+        <Background>
+            <Container>
+                <Title>Agendamentos</Title>
+
+                <List
+                    data={appointments}
+                    keyExtractor={(item) => String(item.id)}
+                    renderItem={({item}) => (
+                        <Appointment
+                            onCancel={() => handleCancel(item.id)}
+                            data={item}
+                        />
+                    )}
+                />
+            </Container>
+        </Background>
+    );
 };
 
 export default Dashboard;
